e92b757647538da3022da7226d82c747d21a88b1
dhruvsoni0612 <75793033+dhruvsoni0612@users.noreply.github.com>
2023-07-05 12:08:42 +0530

    Update page.js

--- app/search/results/page.js
+++ app/search/results/page.js
@@ -1,7 +1,7 @@
 import { ResultsList } from "../components/ResultsList";
 
 export default async function Page() {
-    const res = await fetch('http://localhost:3000/api/search');
+    const res = await fetch('/api/search');
     const data = await res.json();
 
     return (
