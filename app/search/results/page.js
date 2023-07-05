47575ca028fb2bf03fce328345839bf23be3761c
dhruvsoni0612 <75793033+dhruvsoni0612@users.noreply.github.com>
2023-07-05 15:01:30 +0530

    Update page.js

--- app/search/results/page.js
+++ app/search/results/page.js
@@ -1,7 +1,7 @@
 import { ResultsList } from "../components/ResultsList";
 
 export default async function Page() {
-    const res = await fetch(https://airbnb-clone.vercel.app/api/search');
+    const res = await fetch('http://localhost:3000/api/search');
     const data = await res.json();
 
     return (
